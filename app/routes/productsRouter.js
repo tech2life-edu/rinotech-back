df2a7984d33796a367eb8658bbf7c97a9142d40c
Cristian Rodriguez <cristianrodrig149@gmail.com>
2022-12-02 23:59:46 +0100

    ✨feat(routes): add http methods to productsRouter

---
+++ app/routes/productsRouter.js
@@ -0,0 +1,22 @@
+import express from 'express';
+
+const router = express.Router();
+
+
+router.get('/');
+
+router.get('/:id');
+
+router.post('/');
+
+router.patch('/:id');
+
+router.delete('/:id');
+
+
+export default router;
+
+
+
+
+
