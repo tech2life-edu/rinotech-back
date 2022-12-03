b3c7ad7625afe392cb34299f37036823672a3e8e
Cristian Rodriguez <cristianrodrig149@gmail.com>
2022-12-03 01:12:51 +0100

    🎨feat(models): Improve the product schema by adding new features

--- app/models/products.js
+++ app/models/products.js
@@ -1,26 +1,34 @@
 import mongoose from 'mongoose';
 
-
-const ProductScheme = new mongoose.Schema({
-    name:{
-        type: String,
-    },
-    price:{
-        type: Number,
-    },
-    image:{
+const ProductScheme = new mongoose.Schema(
+  {
+    name: {
       type: String,
-    }
-
-
-
-
-},
-{
-  timestamps: true,
-  versionKey: false
-}
-
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type:Array,
+    },
+    primaryImage: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: Array,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
 );
 
 export default mongoose.model('Products', ProductScheme);
