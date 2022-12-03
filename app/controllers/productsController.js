909a26a26b0ae51b35f63275722767515d061d65
Cristian Rodriguez <cristianrodrig149@gmail.com>
2022-12-03 01:25:03 +0100

    ✨feat(controller):Add product controller

---
+++ app/controllers/productsController.js
@@ -0,0 +1,26 @@
+const getProducts = async () => {
+
+}
+
+const getProduct = async () => {}
+
+const createProduct = async () => {}
+
+const updateProduct = async () => {}
+
+const deleteProduct = async () => {}
+
+
+
+
+export default {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+}
+
+
+
+
