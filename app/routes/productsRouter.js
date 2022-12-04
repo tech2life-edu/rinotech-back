3181de0aaace79dcc2c0996eb5a7c6b7086b789c
Cristian Rodriguez <cristianrodrig149@gmail.com>
2022-12-04 12:33:36 +0100

    ✨feat:add product controller to the productsRouter

--- app/routes/productsRouter.js
+++ app/routes/productsRouter.js
@@ -1,22 +1,22 @@
 import express from 'express';
+import {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from '../controllers/productsController.js';
 
 const router = express.Router();
 
+router.get('/', getProducts);
 
-router.get('/');
+router.get('/:id', getProduct);
 
-router.get('/:id');
+router.post('/', createProduct);
 
-router.post('/');
+router.patch('/:id', updateProduct);
 
-router.patch('/:id');
-
-router.delete('/:id');
-
+router.delete('/:id', deleteProduct);
 
 export default router;
-
-
-
-
-
