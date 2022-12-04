919b30af166e45c322e270e67a261586ed001f68
Cristian Rodriguez <cristianrodrig149@gmail.com>
2022-12-04 12:28:11 +0100

    ✨feat:import routes add Middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './config/mongo.js'
+import Routes from './app/routes/index.js'
 
 
 dotenv.config()
@@ -9,6 +10,9 @@ const app = express()
 const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
+app.use('api/1.0/', Routes)
+
+
 
 
 
