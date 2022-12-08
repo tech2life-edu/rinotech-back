8c03f87808ce8ca7f0de2543c92d21806a1ea2d6
Cristian Rodriguez <cristianrodrig149@gmail.com>
2022-12-08 12:47:31 +0100

    ✨feat: Adding crud logic

--- app/controllers/productsController.js
+++ app/controllers/productsController.js
@@ -1,26 +1,47 @@
-const getProducts = async () => {
+import { httpError } from "../helpers/handleError.js";
+import productsModel from "../models/products.js";
 
-}
+const getProducts = async (req, res) => {
+  try {
+    const products = await productsModel.find();
+    res.status(200).json({ message: 'Get all products.', products });
+  } catch (err) {
+    httpError(res, err);
+  }
+};
 
-const getProduct = async () => {}
+const getProduct = (req, res) => {
+  res.status(200).json({ message: 'Get a product.' });
+};
 
-const createProduct = async () => {}
+const createProduct = async (req, res) => {
+  try {
+    const { name, price, description, category ,primaryImage,images } = req.body;
+    const newProduct = await productsModel.create({
+      name,
+      price,
+      description,
+      category,
+      primaryImage,
+      images
 
-const updateProduct = async () => {}
-
-const deleteProduct = async () => {}
-
-
-
-
-export default {
-  getProducts,
-  getProduct,
-  createProduct,
-  updateProduct,
-  deleteProduct,
-}
-
+    });
+    
 
 
+    res.status(201).json({ message: 'Product created.', newProduct });
+  } catch (err) {
+    httpError(res, err);
+  }
+  }
 
+
+const updateProduct = (req, res) => {
+  res.status(200).json({ message: 'Update a product.' });
+};
+
+const deleteProduct = (req, res) => {
+  res.status(200).json({ message: 'Delete a product.' });
+};
+
+export { getProducts, getProduct, createProduct, updateProduct, deleteProduct };
