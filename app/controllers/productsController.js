affceb8a8966ca77a20f21c8daf6d4837385b66e
Cristian Rodriguez <cristianrodrig149@gmail.com>
2022-12-09 18:09:08 +0100

    ✨feat: Add updateProduct deleteProduct

--- app/controllers/productsController.js
+++ app/controllers/productsController.js
@@ -1,6 +1,12 @@
-import { httpError } from "../helpers/handleError.js";
-import productsModel from "../models/products.js";
+import { httpError } from '../helpers/handleError.js';
+import productsModel from '../models/products.js';
 
+/**
+ * It's an async function that uses the productsModel to find all products and then sends a response
+ * with a message and the products.
+ * @param req - The request object.
+ * @param res - The response object.
+ */
 const getProducts = async (req, res) => {
   try {
     const products = await productsModel.find();
@@ -9,39 +15,95 @@ const getProducts = async (req, res) => {
     httpError(res, err);
   }
 };
+/************************************************************************* */
 
-const getProduct = (req, res) => {
-  res.status(200).json({ message: 'Get a product.' });
+/**
+ * It gets a product by id.
+ * @param req - The request object.
+ * @param res - the response object
+ */
+const getProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await productsModel.findById(id);
+    res.status(200).json({ message: 'Get a product.', product });
+  } catch (err) {
+    httpError(res, err);
+  }
 };
 
+/******************************************************************************* */
+
+/**
+ * It creates a new product in the database
+ * @param req - {
+ * @param res - response object
+ */
 const createProduct = async (req, res) => {
   try {
-    const { name, price, description, category ,primaryImage,images } = req.body;
+    const { name, price, description, category, primaryImage, images } =
+      req.body;
     const newProduct = await productsModel.create({
       name,
       price,
       description,
       category,
       primaryImage,
-      images
-
+      images,
     });
-    
-
 
     res.status(201).json({ message: 'Product created.', newProduct });
   } catch (err) {
     httpError(res, err);
   }
-  }
-
-
-const updateProduct = (req, res) => {
-  res.status(200).json({ message: 'Update a product.' });
 };
 
-const deleteProduct = (req, res) => {
-  res.status(200).json({ message: 'Delete a product.' });
+/******************************************************************************* */
+
+/**
+ * It updates a product in the database
+ * @param req - {
+ * @param res - response object
+ */
+const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, price, description, category, primaryImage, images } =
+      req.body;
+    let updatedProduct = await productsModel.findByIdAndUpdate(
+      id,
+      {
+        name,
+        price,
+        description,
+        category,
+        primaryImage,
+        images,
+      },
+      { new: true }
+    );
+    res.status(200).json({ message: 'Product updated.', updatedProduct });
+  } catch (err) {
+    httpError(res, err);
+  }
+};
+
+/******************************************************************************* */
+
+/**
+ * It deletes a product from the database.
+ * @param req - The request object.
+ * @param res - the response object
+ */
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedProduct = await productsModel.findByIdAndDelete(id);
+    res.status(200).json({ message: 'Product deleted.', deletedProduct });
+  } catch (err) {
+    httpError(res, err);
+  }
+
 };
 
 export { getProducts, getProduct, createProduct, updateProduct, deleteProduct };
